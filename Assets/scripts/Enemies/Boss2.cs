5082498334c940c428d0eaa5aa6bc24e5c18fa22
fitoski <129151528+fitoski@users.noreply.github.com>
2023-11-22 13:02:34 +0300

    31
    2. boss düzelt

---
+++ Assets/scripts/Enemies/Boss2.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2 : Enemy
+{
+    public float attackRange = 5f;
+    public int attackDamage = 10;
+    public float attackCooldown = 5f;
+    private float attackTimer;
+
+    private Transform playerTransform;
+    private Animator bossAnimator;
+    //private BossAbilities bossAbilities;
+    private bool isAttacking = false;
+
+    public List<DropItem> droppableItems;
+
+    private new void Start()
+    {
+        base.Start();
+        maxHealth = 10;
+        speed = 2f;
+        baseDamage = 1;
+        scoreValue = 15;
+        experiencePointsValue = 10;
+        damageMultiplierPerWave = 1.5f;
+        currentHealth = maxHealth;
+        base.Start();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        bossAnimator = GetComponent<Animator>();
+        attackTimer = attackCooldown;
+    }
+
+    private new void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            bossAnimator.SetTrigger("Die");
+            return;
+        }
+
+        float distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
+
+        if (!isAttacking && distanceToPlayer <= attackRange && attackTimer <= 0)
+        {
+            AttackPlayer();
+        }
+        else if (!isAttacking)
+        {
+            FollowPlayer();
+        }
+
+        if (attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime;
+        }
+    }
+
+    void FollowPlayer()
+    {
+        if (!isAttacking)
+        {
+            UpdateOrientationTowardsPlayer();
+            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        }
+    }
+
+    void AttackPlayer()
+    {
+        UpdateOrientationTowardsPlayer();
+        isAttacking = true;
+        attackTimer = attackCooldown;
+
+        int attackType = UnityEngine.Random.Range(0, 2); 
+        if (attackType == 0)
+        {
+            AttackType1();
+        }
+        else
+        {
+            AttackType2();
+        }
+    }
+
+    void AttackType1()
+    {
+        Debug.Log("Boss is performing attack type 1");
+        bossAnimator.SetTrigger("Attack1");
+    }
+
+    void AttackType2()
+    {
+        Debug.Log("Boss is performing attack type 2");
+        bossAnimator.SetTrigger("Attack2");
+    }
+
+    void UpdateOrientationTowardsPlayer()
+    {
+        bool shouldFaceRight = playerTransform.position.x > transform.position.x;
+        GetComponent<SpriteRenderer>().flipX = shouldFaceRight;
+    }
+
+    public void OnAttackEnd()
+    {
+        Debug.Log("Attack ended");
+        bossAnimator.SetBool("isAttacking", false);
+        isAttacking = false;
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        DropItemOnDeath();
+    }
+
+    void DropItemOnDeath()
+    {
+        Debug.Log("item düştü");
+        foreach (DropItem item in droppableItems)
+        {
+            int randomChance = UnityEngine.Random.Range(0, 100);
+            Debug.Log("random chance: " + randomChance + "item drop chance: " + item.dropChance);
+
+            if (randomChance < item.dropChance)
+            {
+                Debug.Log("dropped item: " + item.itemName);
+                Instantiate(item.itemPrefab, transform.position, Quaternion.identity);
+                break;
+            }
+        }
+    }
+}
